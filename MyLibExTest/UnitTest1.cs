e8fb708eb42f406abfb514dff102a620febbc40b
Orange <kkchristoph@outlook.com>
2025-01-24 14:46:55 -0500

    modify lab2

--- MyLibExTest/UnitTest1.cs
+++ MyLibExTest/UnitTest1.cs
@@ -11,7 +11,34 @@ public class UnitTest1
         int a = 2; int b = 3; //arrange
         int expected = 6;
 
-        int actual = LibEx.MyLibEx(2,3);
+        int actual = LibEx.MyLibEx(a,b);
+        Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void Test2()
+    {
+        int a = 4; int b = 5; //arrange
+        int expected = 20;
+
+        int actual = LibEx.MyLibEx(a,b);
+        Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void Test3()
+    {
+        int a = 0; int b = 20; //arrange
+        int expected = 0;
+
+        int actual = LibEx.MyLibEx(a,b);
+        Assert.Equal(expected, actual);
+    }
+    [Fact]
+    public void Test4()
+    {
+        int a = 9; int b = -3; //arrange
+        int expected = -27;
+
+        int actual = LibEx.MyLibEx(a,b);
         Assert.Equal(expected, actual);
     }
 }
