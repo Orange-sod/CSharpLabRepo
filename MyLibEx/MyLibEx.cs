a92117bf55a6e1172dd32b19114b315391589cd9
Orange <kkchristoph@outlook.com>
2025-01-24 14:36:54 -0500

    lab 2 commit

---
+++ MyLibEx/MyLibEx.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLibEx;
+
+public class LibEx
+{
+    public static int MyLibEx(int num1, int num2){
+        int ex = num1 * num2;
+        Console.WriteLine($"The result of multiplying two num is {ex} ");
+        return ex;
+    }
+}
---
+++ MyLibExTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using MyLibEx;
+
+namespace MyLibExTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        int a = 2; int b = 3; //arrange
+        int expected = 6;
+
+        int actual = LibEx.MyLibEx(2,3);
+        Assert.Equal(expected, actual);
+    }
+}
